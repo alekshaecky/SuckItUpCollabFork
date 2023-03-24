357ac689b0ab79a74d19a1da14a5612fc0410ff8
Michael Kapuniai <82479861+Kapuniai@users.noreply.github.com>
2023-03-23 18:30:42 -0700

    Add files via upload

---
+++ SuckItUpUnityProject/Assets/3rdParty/Mini First Person Controller/Scripts/Components/Zoom.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Zoom : MonoBehaviour
+{
+    Camera mCamera;
+    public float defaultFOV = 60;
+    public float maxZoomFOV = 15;
+    [Range(0, 1)]
+    public float currentZoom;
+    public float sensitivity = 1;
+
+
+    void Awake()
+    {
+        // Get the camera on this gameObject and the defaultZoom.
+        mCamera = GetComponent<Camera>();
+        if (mCamera)
+        {
+            defaultFOV = mCamera.fieldOfView;
+        }
+    }
+
+    void Update()
+    {
+        // Update the currentZoom and the camera's fieldOfView.
+        currentZoom += Input.mouseScrollDelta.y * sensitivity * .05f;
+        currentZoom = Mathf.Clamp01(currentZoom);
+        Camera.main.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
+    }
+}

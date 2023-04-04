6b6a91a6d6acd18a1d028bc4d7f331a2188d5934
Jesse B <125101471+mr5x5x5@users.noreply.github.com>
2023-04-04 15:26:23 -0700

    Vending Machine Upgrade script
    Updated Vending machine with placeholder code for Nozzle Upgrades. Also added some Enemies to Test area of ForceCapacity Scene.

---
+++ SuckItUpUnityProject/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // check Nozzle level
+        if (PlayerPrefs.GetInt("PrefsTotalScore") < 10000)
+        {
+            /* 
+            PlayerPrefs.SetInt("PrefsCurrentVacuumPower") = 1;
+            */
+        }
+        if (PlayerPrefs.GetInt("PrefsTotalScore") < 20000)
+        {
+            /*
+            PlayerPrefs.SetInt("PrefsCurrentVacuumPower") = 2;
+            */
+        }
+        if (PlayerPrefs.GetInt("PrefsTotalScore") < 25000)
+        {
+            /*
+            PlayerPrefs.SetInt("PrefsCurrentVacuumPower") = 3;
+            */
+        }
+        if (PlayerPrefs.GetInt("PrefsTotalScore") < 30000)
+        {
+            /*
+            PlayerPrefs.SetInt("PrefsCurrentVacuumPower") = 4;
+            */
+        }
+        // else bigger than 50000
+        /*
+        PlayerPrefs.SetInt("PrefsCurrentVacuumPower") = 5;
+        */
+
+    }
+
+}

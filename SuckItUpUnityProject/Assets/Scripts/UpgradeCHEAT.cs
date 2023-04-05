0090363e1bae0670e50c942981707a663297aaa6
Aleks <vhaecky@gmail.com>
2023-04-04 19:08:26 -0700

    Add chunk to Sandbox
    Also, add an UgradeCHEAT object for testing.

---
+++ SuckItUpUnityProject/Assets/Scripts/UpgradeCHEAT.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeCHEAT : MonoBehaviour
+{
+
+    public AudioClip BlingSFX;                   // vacuum sound
+    int BlingAudioIndex;                  // SoundBoard audio index
+    private bool collidable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlingAudioIndex = SoundBoard.Instance.AddSoundEffect(BlingSFX);
+        collidable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        //  This code can be simplified but I can place 23 pieces of junk in that time.
+        if (collidable)
+        {
+            collidable = false;
+            switch (PlayerPrefs.GetInt("PrefsCurrentVacuumPower"))
+            {
+                case 1:
+                    PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 2);
+                    break;
+                case 2:
+                    PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 3);
+                    break;
+                case 3:
+                    PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 4);
+                    break;
+                case 4:
+                    PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 5);
+                    break;
+                default:
+                    PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 1);
+                    break;
+            }
+            SoundBoard.Instance.PlaySFX(BlingAudioIndex);
+        }
+        collidable = false;
+
+        Debug.Log("Current Nozzle: " + PlayerPrefs.GetInt("PrefsCurrentVacuumPower"));
+
+        // Need to prevent multiple triggering events.
+        // So we add a cooldown.
+        StartCoroutine(Reset());
+    }
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(5);
+        collidable = true;
+    }
+
+}

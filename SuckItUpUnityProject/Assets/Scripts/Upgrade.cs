3b3c1051618e678265dbfb161137efc587048215
Aleks <vhaecky@gmail.com>
2023-04-26 10:15:50 -0700

    Adjust Upgrade ranges.

--- SuckItUpUnityProject/Assets/Scripts/Upgrade.cs
+++ SuckItUpUnityProject/Assets/Scripts/Upgrade.cs
@@ -27,6 +27,8 @@ public class Upgrade : MonoBehaviour
     {
         
     }
+
+    // Old version of this function. 
     //private void OnTriggerEnter(Collider other)
     //{
     //    // check Nozzle level
@@ -54,27 +56,33 @@ public class Upgrade : MonoBehaviour
     //    StartCoroutine("waitABit");
     //}
 
+
+    // Fixed version with correct logic. 
     private void OnTriggerEnter(Collider other)
     {
         float score = PlayerPrefs.GetInt("PrefsTotalScore");
 
-        if (score >= 40000 ) // 40000+ 
+        //if (score >= 40000 ) // 40000+ 
+        if (score >= 250000) // 250000+ 
         {
             PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 5);
         }
-        else if (score >= 30000) //30000 - 39000 
+        //else if (score >= 30000) //30000 - 39000 
+        else if (score >= 50000) //50000 - 249999
         {
             PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 4);
         }
-        else if (score >= 20000) // 20000 - 29999
+        //else if (score >= 20000) // 20000 - 29999
+        else if (score >= 10000) // 10000 - 49999
         {
             PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 3);
         }
-        else if (score >= 10000) // 10000 - 19999
+        //else if (score >= 10000) // 10000 - 19999
+        else if (score >= 1000) // 1000 - 9999
         {
             PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 2);
         }
-        else // 0 - 9999
+        else // 0 - 999
         {
             PlayerPrefs.SetInt("PrefsCurrentVacuumPower", 1);
         }

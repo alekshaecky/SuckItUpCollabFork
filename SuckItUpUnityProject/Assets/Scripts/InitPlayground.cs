97ee25906e7ab2cc94e6a0ba2a0d7d2d3a6eb374
Aleks <vhaecky@gmail.com>
2023-05-01 17:53:09 -0700

    Bug fixes; reactivate and tweak donut horizontal platform

--- SuckItUpUnityProject/Assets/Scripts/InitPlayground.cs
+++ SuckItUpUnityProject/Assets/Scripts/InitPlayground.cs
@@ -8,12 +8,17 @@ public class InitPlayground : MonoBehaviour
     void Start()
     {
         // If Nozzle Level is less than 5, deactivate the teleporter and final exit. 
-        if (PlayerPrefs.GetInt("PrefsCurrentVacuumPower") != 5)
+        if (PlayerPrefs.GetInt("PrefsCurrentVacuumPower") < 5)
         {
             // Disable final teleporter
             GameObject.Find("TeleporterFinal").SetActive(false);
             // Disable final exit
             GameObject.Find("LevelExitArchFinal").SetActive(false);
+        }
+        // If Nozzle Level is less than 3, deactivate ObstacleCourse shortcut teleporter. 
+        if (PlayerPrefs.GetInt("PrefsCurrentVacuumPower") < 3)
+        {
+            GameObject.Find("TeleporterObstacleShortcut").SetActive(false);
         }
     }
 
